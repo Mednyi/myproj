d4327336741e5f19e83ec4771648a8f7bddc4d73
mytestkube <mednyi.vsadnik@gmail.com>
2019-11-22 21:57:21 +0300

    jwt_2

--- routes/users.js
+++ routes/users.js
@@ -3,28 +3,105 @@ var router = express.Router();
 const uuid = require('uuid')
 let users = require('../models/users')
 let films = require('../models/films')
+const jose = require('jose')
+const {
+  JWE,   // JSON Web Encryption (JWE)
+  JWK,   // JSON Web Key (JWK)
+  JWKS,  // JSON Web Key Set (JWKS)
+  JWS,   // JSON Web Signature (JWS)
+  JWT,   // JSON Web Token (JWT)
+  errors // errors utilized by jose
+} = jose
+const key = JWK.asKey("HelloMyLilFriend")
 /* GET users listing. */
 router.post('/login', function(req, res, next) {
   const user = users.find(item => item.name === req.body.name)
   if(user && user.password === req.body.password) {
-    user.token = uuid()
-    user.exp = new Date()
-    user.exp.setMinutes(user.exp.getMinutes() + 120)
-    res.send(user.token);
+    const token = JWT.sign({
+      iss: "ImCoder",
+      sub: 'Auth',
+      aud: user.id,
+      iat: new Date(),
+      ttype: 'access',
+      header: {
+        typ: "JWT",
+        alg: "HS256"
+      },
+      jti: uuid() 
+    },key, {
+      expiresIn: "2m",
+      algorithm: "HS256"
+    })
+    const refresh = JWT.sign({
+      iss: "ImCoder",
+      sub: 'Auth',
+      ttype: 'refresh',
+      iat: new Date(),
+      aud: user.id,
+      header: {
+        typ: "JWT",
+        alg: "HS256"
+      },
+      jti: uuid() 
+    },key, {
+      expiresIn: "5m",
+      algorithm: "HS256"
+    })
+    res.send(JSON.stringify({token, refresh}))
   } else {
     res.status(401).send("Unauthorized")
   }
 });
 let auth = async (req, res, next) => {
   const user = users.find(item => item.id === req.params.id)
-  if(user && req.body.token === user.token && new Date() < user.exp) {
+  const token = req.headers.authorization.split(' ')[1]
+  try {
+    user && JWT.verify(token, key, {
+      algorithms: ["HS256"],
+      issuer: "ImCoder",
+      subject: "Auth",
+      audience: req.params.id
+    })
     next()
-  } else {
-    delete user.token
-    res.status(401).send() 
+  } catch (e) {
+    console.log(e.message)
+    res.status(401).send("token is invalid") 
   }
 }
 router.use('/:id', auth)
+router.post('/:id/refresh', function(req, res, next) {
+  const token = JWT.sign({
+    iss: "ImCoder",
+    sub: 'Auth',
+    aud: user.id,
+    iat: new Date(),
+    ttype: 'access',
+    header: {
+      typ: "JWT",
+      alg: "HS256"
+    },
+    jti: uuid() 
+  },key, {
+    expiresIn: "2m",
+    algorithm: "HS256"
+  })
+  const refresh = JWT.sign({
+    iss: "ImCoder",
+    sub: 'Auth',
+    ttype: 'refresh',
+    iat: new Date(),
+    aud: user.id,
+    header: {
+      typ: "JWT",
+      alg: "HS256"
+    },
+    jti: uuid() 
+  },key, {
+    expiresIn: "30m",
+    algorithm: "HS256"
+  })
+  res.send(JSON.stringify({token, refresh}))
+});
 router.get('/', function(req, res, next) {
   res.send(users);
 });

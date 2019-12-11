fa6d4ae941dedcfc2a9fa4b71097ac8063936f31
mytestkube <mednyi.vsadnik@gmail.com>
2019-12-11 22:29:48 +0300

    mongodb_API

--- routes/users.js
+++ routes/users.js
@@ -4,10 +4,11 @@ const uuid = require('uuid')
 let users = require('../models/users')
 let films = require('../models/films')
 const auth = require('../modules/auth')
+let mongo = require('../modules/mongo')
 /* GET users listing. */
-router.post('/login', function(req, res, next) {
+router.post('/login', async function(req, res, next) {
   try{
-    let tokens = auth.authorize(req.body.name, req.body.password)
+    let tokens = await auth.authorize(req.body.name, req.body.password)
     res.send(JSON.stringify(tokens))
   } catch (e) {
     res.status(401).send("Unauthorized")
@@ -16,40 +17,54 @@ router.post('/login', function(req, res, next) {
 let authREST = async (req, res, next) => {
   const token = req.headers.authorization.split(' ')[1]
   try {
-    auth.checkAuth(token, req.params.id)
+    await auth.checkAuth(token, req.params._id)
     next()
   } catch (e) {
     console.log(e.message)
     res.status(401).send("token is invalid") 
   }
 }
-router.use('/:id', authREST)
-router.post('/:id/refresh', function(req, res, next) {
-  let tokens = auth.authorize(req.body.name, req.body.password)
+router.use('/:_id', authREST)
+router.post('/:_id/refresh', async function(req, res, next) {
+  let tokens = await auth.authorize(req.body.name, req.body.password)
   res.send(JSON.stringify(tokens))
 });
-router.get('/', function(req, res, next) {
-  res.send(users);
+router.get('/', async function(req, res, next) {
+  try {
+    const users = await mongo.findUsers({})
+    res.send(users);
+  } catch (e) {
+    res.status(500).send(e.message)
+  } 
 });
-router.post('/', (req,res,next) => {
-  let user = {
-    id: uuid(),
-    name: req.body.name,
-    password: req.body.password
+router.post('/', async (req,res,next) => {
+  try {
+    let user = {
+      name: req.body.name,
+      password: req.body.password
+    }
+    user = await mongo.addUser(user)
+    res.send(user)
+  } catch (e) {
+    res.status(500).send(e.message)
   }
-  users.push(user)
-  res.send(user)
 })
-router.delete('/:id', (req,res,next) => {
-  users = users.filter(user => user.id !== req.params.id)
-  res.redirect("/").send("User deleted")
+router.delete('/:_id', async (req,res,next) => {
+  try {
+    await mongo.removeUser({_id: req.params._id})
+    res.redirect("/").send("User deleted")
+  } catch (e) {
+    res.status(500).send(e.message)
+  }
 })
-router.put('/:id', (req,res,next) => {
-  let updated = req.body
-  updated = req.params.id
-  users = users.filter(user => user.id !== req.params.id)
-  users.push(updated)
-  res.send(updated)
+router.put('/:_id', async (req,res,next) => {
+  try {
+    let updated = req.body
+    await mongo.updateUser({_id: req.params._id}, updated)
+    res.send(updated)
+  } catch (e) {
+    res.status(500).send(e.message)
+  }
 })
 
 
@@ -59,9 +74,14 @@ router.post('/:id/films', (req,res) => {
   buyer.films || (buyer.films = [])
   buyer.films.push(bought)
 })
-router.get('/:id', function(req, res, next) {
-  const user = users.find(item => item.id === req.params.id)
-  res.send(JSON.stringify(user));
-  next()
+router.get('/:_id', async function(req, res, next) {
+  try {
+    const user = await mongo.findUsers({_id: req.params._id})[0]
+    res.send(JSON.stringify(user));
+    next()
+  } catch (e) {
+    res.status(500).send(e.message)
+  }
+
 });
 module.exports = router;
